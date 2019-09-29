33d838f1bf1f58a8c2b4eddaea295dea2a200e3d
nightwarrior-xxx <amanv1999@gmail.com>
2019-09-29 09:49:51 +0530

    Modified functionality for followers and following system

--- social/accounts/urls.py
+++ social/accounts/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import (
     show_profile,
     feed,
-    follow_list, 
+    follows_list, 
     followers_list,
     follows,
     stop_follow
@@ -13,8 +13,8 @@ app_name= 'accounts'
 urlpatterns = [
     path('', feed, name='feed'),
     path('<str:username>/', show_profile, name="profile"),
-    path('<str:username>/follow', follow_list, name='follow_list'),
-    path('<str:username>/followers', followers_list, name='followers_list'),
+    path('<str:username>/followed_to', follows_list, name='follow_list'),
+    path('<str:username>/followed_by', followers_list, name='followers_list'),
     path('<str:username>/follows', follows, name='follows'),
     path('<str:username>/stop_follow', stop_follow, name='stop_follow')
 ]

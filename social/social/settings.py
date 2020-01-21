4511d31d3e80086f92c6b81e0475aedc85a37d73
nightwarrior-xxx <amanv1999@gmail.com>
2020-01-21 23:15:55 +0530

    [Fixed] ProfileQuerySet error and [Moved] database to sqlite3

--- social/social/settings.py
+++ social/social/settings.py
@@ -86,12 +86,8 @@ WSGI_APPLICATION = 'social.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.environ['DB_NAME'],
-        'USER': os.environ['DB_USER'],
-        'PASSWORD': os.environ['DB_PASSWORD'],
-        'HOST': os.environ['DB_HOST'],
-        'PORT': ''
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
